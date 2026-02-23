36a0eace114dc51f9cce18235e4997a921798141
Shivam Roy <vn10212@heb.com>
2026-02-23 11:44:58 +0530

    added DP problems

---
+++ src/main/java/org/example/dp/FloydWarshall.java
@@ -0,0 +1,46 @@
+package org.example.dp;
+
+public class FloydWarshall {
+
+    static final int INF = 99999;
+
+    public static void main(String[] args) {
+
+        int[][] graph = {
+                {0, 5, INF, 10},
+                {INF, 0, 3, INF},
+                {INF, INF, 0, 1},
+                {INF, INF, INF, 0}
+        };
+
+        floydWarshall(graph);
+    }
+
+    public static void floydWarshall(int[][] graph) {
+
+        int V = graph.length;
+        int[][] dist = new int[V][V];
+
+        for (int i = 0; i < V; i++)
+            for (int j = 0; j < V; j++)
+                dist[i][j] = graph[i][j];
+
+        for (int k = 0; k < V; k++) {
+            for (int i = 0; i < V; i++) {
+                for (int j = 0; j < V; j++) {
+
+                    if (dist[i][k] + dist[k][j] < dist[i][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                System.out.print(dist[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
